2a27a0873d38acdd1f4329f6f7b5440df4649f99
Nicolas HERVE <n.herve@laposte.net>
2011-07-06 18:29:35 +0200

    Viewer

--- NHerveToolboxMain/src/plugins/nherve/toolbox/genericgrid/SomeStandardThumbnails.java
+++ NHerveToolboxMain/src/plugins/nherve/toolbox/genericgrid/SomeStandardThumbnails.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2010, 2011 Institut Pasteur.
+ * 
+ * This file is part of NHerve Main Toolbox, which is an ICY plugin.
+ * 
+ * NHerve Main Toolbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NHerve Main Toolbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NHerve Main Toolbox. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package plugins.nherve.toolbox.genericgrid;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
 
 import plugins.nherve.toolbox.image.toolboxes.SomeImageTools;
 
@@ -15,7 +36,7 @@ public class SomeStandardThumbnails {
 		createErrorImage();
 	}
 
-	public static void paintError(final Graphics2D g2, final GridCell c) {
+	public static void paintError(final Graphics2D g2, final JComponent c) {
 		int w = c.getWidth();
 		int h = c.getHeight();
 		
