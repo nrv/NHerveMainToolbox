b0e3a2015b8b3ac61dbd6f6bb2ecc0ba78a7fcdb
Nicolas HERVE <n.herve@laposte.net>
2011-08-04 13:45:41 +0200

    Logo in title pane

--- NHerveToolboxMain/src/plugins/nherve/toolbox/plugin/MyFrame.java
+++ NHerveToolboxMain/src/plugins/nherve/toolbox/plugin/MyFrame.java
@@ -10,8 +10,10 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RadialGradientPaint;
 import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
@@ -33,11 +35,13 @@ public class MyFrame extends IcyFrame {
 		final String pluginVersion;
 		final Font nameFont;
 		final Font versionFont;
+		final Image logo;
 
-		public MyLogo(String pluginName, String pluginVersion, Dimension dim) {
+		public MyLogo(String pluginName, String pluginVersion, Dimension dim, Image logo) {
 			super();
 			this.pluginName = pluginName;
 			this.pluginVersion = pluginVersion;
+			this.logo = logo;
 			setPreferredSize(dim);
 			nameFont = new Font("Arial", Font.BOLD, 20);
 			versionFont = new Font("Arial", Font.ITALIC, 10);
@@ -81,12 +85,16 @@ public class MyFrame extends IcyFrame {
 			g2.setColor(darkColor.darker().darker());
 			g2.setFont(nameFont);
 			Rectangle2D ts = g2.getFontMetrics().getStringBounds(pluginName, g2);
-			g2.drawString(pluginName, (int) ((w - ts.getWidth()) / 2), (int) (h - ts.getHeight() / 2));
+			g2.drawString(pluginName, (int) ((w - ts.getWidth()) / 2), (int) ((h + ts.getHeight()) / 2));
 			
 			g2.setColor(lightColor.brighter().brighter());
 			g2.setFont(versionFont);
 			ts = g2.getFontMetrics().getStringBounds(pluginVersion, g2);
 			g2.drawString(pluginVersion, (int) (w - ts.getWidth() - 5), h - 5);
+			
+			if (logo != null) {
+				g2.drawImage (logo, 5, 5, null);
+			}
 
 			g2.dispose();
 		}
@@ -97,31 +105,28 @@ public class MyFrame extends IcyFrame {
 	}
 	
 	public static MyFrame create(SingletonPlugin plugin, String title) {
-		return create(plugin, title, true, true, true, true);
+		return create(plugin, title, true, true, true, true, true);
 	}
 
-	public static MyFrame create(SingletonPlugin plugin, String title, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable) {
-		final MyFrame result = new MyFrame(plugin.getName(), plugin.getVersion(), title, new Dimension(400, 50), resizable, closable, maximizable, iconifiable);
+	public static MyFrame create(SingletonPlugin plugin, String title, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable, boolean showLogo) {
+		Image logo = showLogo ? plugin.getDescriptor().getIconAsImage() : null;
+		final MyFrame result = new MyFrame(plugin.getName(), plugin.getVersion(), title, new Dimension(400, 74), resizable, closable, maximizable, iconifiable, logo);
 		result.setVisible(true);
-
 		return result;
 	}
 	
 	public static MyFrame create(SingletonPlugin plugin, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable) {
-		final MyFrame result = new MyFrame(plugin.getName(), plugin.getVersion(), plugin.getName(), new Dimension(400, 50), resizable, closable, maximizable, iconifiable);
-		result.setVisible(true);
-
-		return result;
+		return create(plugin, plugin.getName(), resizable, closable, maximizable, iconifiable, true);
 	}
 
 	protected final JPanel internalPanel;
 
-	public MyFrame(String pluginName, String pluginVersion, String title, Dimension dim, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable) {
+	public MyFrame(String pluginName, String pluginVersion, String title, Dimension dim, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable, Image logo) {
 		super(title, resizable, closable, maximizable, iconifiable);
 
 		setLayout(new BorderLayout());
 
-		add(new MyLogo(pluginName, pluginVersion, dim), BorderLayout.NORTH);
+		add(new MyLogo(pluginName, pluginVersion, dim, logo), BorderLayout.NORTH);
 
 		internalPanel = new JPanel();
 		internalPanel.setOpaque(false);
